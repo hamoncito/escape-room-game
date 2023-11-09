605a17b1fdd5ea4ee9f9c5f1f3cf7c2c499bf7b3
hamoncito <popiel.agn@gmail.com>
2023-11-09 21:37:38 +0100

    Added interaction system

---
+++ Assets/Scripts/PhysicsPickup.cs
@@ -0,0 +1,42 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PhysicsPickup : MonoBehaviour
+{
+    [SerializeField] private LayerMask isGrabbable;
+    [SerializeField] private Camera playerCamera;
+    [SerializeField] private Transform pickupTarget;
+    [Space]
+    [SerializeField] private float pickupRange;
+    private Rigidbody currObj;
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.GetMouseButtonDown(0)) {
+            if(currObj) {
+                currObj.useGravity = true;
+                currObj = null;
+                return;
+            }
+
+            Ray cameraRay = playerCamera.ViewportPointToRay(new Vector3(0.5f, 0.5f, 0f));
+            if(Physics.Raycast(cameraRay, out RaycastHit HitInfo, pickupRange, isGrabbable)) {
+                currObj = HitInfo.rigidbody;
+                currObj.useGravity = false;
+            }
+        }
+        
+    }
+
+    void FixedUpdate()
+    {
+        if(currObj) {
+            Vector3 directionToPoint = pickupTarget.position - currObj.position;
+            float distanceToPoint = directionToPoint.magnitude;
+
+            currObj.velocity = directionToPoint * 12f * distanceToPoint;
+        }
+    }
+}
